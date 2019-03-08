564942f0534f3f57f234be586603b1272620fabb
Raj <mail2nitinraj@gmail.com>
2019-03-08 06:39:48 +0530

    added more methods

--- playground.js
+++ playground.js
@@ -1,3 +1,126 @@
+/*Number reverse
+
+((num)=>{
+let str = num.toString();
+str = str.split("");
+str = str.reverse();
+str = str.join('');
+str = +str;
+console.log(str, typeof str);
+})(12345);
+/*Number reverse*/
+
+/**check for plaindrome
+
+console.log(((str)=>{
+  str = str.replace(/[' ']/g,'');
+for(let i = 0; i < str.length-1; i++){
+  if(str[i]===str[str.length-1-i] ){
+  }else{
+  return false;
+  }
+}
+return true;
+})('1 2 32    1'));
+/*check for palindrome*/
+
+/*random number between 5 and 7
+(()=>{
+  console.log( 5 +  Math.random() * 2);
+})();
+/**/
+
+/* recursiveFactorial
+function recursiveFactorial(num){
+  if(num===1){
+    return 1;
+  }
+  return num * recursiveFactorial(num-1);
+}
+console.log('Output : '+recursiveFactorial(5));
+/**/
+
+/*recursiveGcd
+function recursiveGcd(a,b){
+  var inp = a < b ? a : b;
+    function chkGcd(inp){
+      if(inp === 1){
+        return 1;
+      }
+      if((a % inp === 0) && (b % inp === 0)){
+         return inp;
+      } else{
+        return  chkGcd(inp-1);
+      }
+    }
+ return chkGcd(inp);
+}
+console.log('Output : '+recursiveGcd(100,200));
+/**/
+
+/*Array binary search
+
+function bSearch(arr,target){
+
+    var half = Math.floor(arr.length / 2);
+    if(arr[half] === target){
+      return half;
+    }
+
+    if(target > arr[half]){
+      return half + bSearch(arr.slice(half,arr.length),target);
+    }else {
+      return bSearch(arr.slice(0, half),target);
+    }
+
+
+}
+var inpArr = [1,22,35,47,58,61,75,86,90];
+
+console.log('Output : ' + bSearch(inpArr,58));
+
+/**/
+
+/*first non repeating char in a string
+console.log(((str)=>{
+  for(let i=0 ; i<str.length ; i++){
+    let compareStr = str.substr(i+1, str.length);
+    if( !(compareStr.indexOf(str[i]) > -1) ){
+      return str[i];
+    }
+  }
+})('the quick brown fox jumps then quickly blow air'));
+
+/**/
+/* array reduce, generator function
+console.log(((arr,str)=>{
+
+  function* loopOver(){
+    yield 5;
+    yield 6;
+  }
+
+let l = loopOver();
+console.log(l.next(), l.next());
+console.log(l.next(), l.next());
+  for(v of loopOver()){
+  console.log(v);
+}
+
+
+  return  "Sum of array : "+arr.reduce((p,v,i)=>{
+      return  p+v;
+    });
+
+
+})([2,3,1,5],'Hello World'));
+/**/
+
+/**/
+
+
+/**/
+
 /*Prime Number using recursion */
 /*
 function chkPrime(nr, inc) {
